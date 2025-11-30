d4c2bb6a0d08e74d48ee4e1c08b2f9206a11b98e
Damon Chase <chasedamon447@gmail.com>
2025-11-30 13:10:08 -0500

    Use SVDTruncater to reduce dimensionality of TF-IDF matrix

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from nltk.corpus import stopwords
 import string
 import re
 from sklearn.cluster import KMeans
+from sklearn.decomposition import TruncatedSVD
+import matplotlib.pyplot as plt
+
 
 STOP_WORDS = set(stopwords.words("english"))
 PUNCTUATION = f"[{string.punctuation}]"
@@ -35,21 +38,24 @@ def main():
     X = vectorizer.fit_transform(posts)
     feature_names = vectorizer.get_feature_names_out()
 
-    # Run KMeans algorithm to generate 4 clusters in vectorized posts
-    kmeans = KMeans(n_clusters=5, random_state=0, n_init="auto").fit(X)
+    # Used TruncatedSVD to reduce the demensionality of TF-IDF matrix X
+    svd = TruncatedSVD(n_components=100, random_state=0)
+    X_reduced = svd.fit_transform(X)
+
+    # Run KMeans algorithm to generate 5 clusters in vectorized posts
+    kmeans = KMeans(n_clusters=5, random_state=0, n_init="auto").fit(X_reduced)
 
     # Print the top terms of each document in each cluster
-    for i in range(1, 5):
+    for i in range(kmeans.n_clusters):
         print(f"================Cluster {i}================")
         mask = kmeans.labels_ == i
         rows_in_cluster = X[mask]
-
-        for j in range(0, 10):
+        
+        for j in range(min(10, rows_in_cluster.shape[0])):
             row = rows_in_cluster[j]
             top3_vals = row.data.argsort()[::-1][:3]
             top_indices = row.indices[top3_vals]
             print(feature_names[top_indices])
-
 
 if __name__ == "__main__":
     main()
